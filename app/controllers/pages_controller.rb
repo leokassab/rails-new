0e07a8b3e0244b3beaf989fc8d1efd9d14f25877
leo kassabi <leokassabi@gmail.com>
2021-12-07 21:07:08 +0100

    2finish

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = params[:answer]
+
+    if @answer == 'I am going to work'
+      @coach_answer = "Great!"
+    elsif @answer.include?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
